6dce24994fc881a467e86e4527b564865686d252
Kaviyadharshini Sankar <skdharshiniswe@gmail.com>
2025-06-13 13:14:24 +0530

    Time: 0 ms (100%), Space: 41.1 MB (47.19%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
@@ -1,13 +1,14 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-        int runningSum = 0;
-        int expectingSum = 0;
-        int chunks =  0;
+        int runningSum = 0; // Cumulative sum of elements seen so far
+        int expectingSum = 0; // Cumulative sum of sorted values (i.e., expected values if sorted)
+        int chunks =  0; // Number of chunks that can be independently sorted
 
         for(int i=0; i<arr.length; i++){
-            runningSum += arr[i];
-            expectingSum += i;
+            runningSum += arr[i]; // Sum of actual elements
+            expectingSum += i;  // Sum of indices (expected values in sorted array)
 
+            // If the sums match, we can split here
             if(runningSum == expectingSum) chunks++;
         }
         return chunks;
