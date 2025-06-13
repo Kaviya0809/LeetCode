aa902167d080744e30fb5dbde9e5a7e072e76a6c
Kaviyadharshini Sankar <skdharshiniswe@gmail.com>
2025-06-13 11:55:50 +0530

    Time: 24 ms (9.41%), Space: 42.5 MB (70.96%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -6,8 +6,13 @@ class Solution {
         for(int i=0; i<s.length(); i++){
             char a = s.charAt(i);
             char b = t.charAt(i);
+
+            // Check if 'a' is already mapped to a different character than 'b'
+            // or if 'b' is already mapped to a different character than 'a'
+
             if(map1.containsKey(a) && map1.get(a)!= b  || map2.containsKey(b) && map2.get(b)!= a) return false;
 
+            // Add the mapping if not already present
             map1.put(a,b);
             map2.put(b,a);
         }
