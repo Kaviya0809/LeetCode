4ab9375b63a916ae188230acd5199077874af7d9
Kaviyadharshini Sankar <skdharshiniswe@gmail.com>
2025-06-15 20:13:51 +0530

    Time: 1 ms (44.58%), Space: 41.2 MB (32.96%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,16 +1,15 @@
 class Solution {
     public int addDigits(int num) {
-        while(num>=10)
-        {
-        int sum=0;
-        while(num!=0)
-        {
-            int r=num%10;
-            sum=sum+r;
-            num=num/10;
-        }
-        num=sum;
-        }
-        return num;
+        //if num is already a single digit
+        if(num/10 == 0) return num;
+        //recursively reduce by summing digits
+        else return addDigits(add(num));
+    }
+
+    public static int add(int num){
+        //last digit
+        if(num/10 == 0) return num;
+        //sum current digit and recurse
+        return num%10 + add(num/10);
     }
 }
